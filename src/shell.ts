986b833015bbe81cb0065f5135c899b07bf597c1
SessionHu <102411014+SessionHu@users.noreply.github.com>
2025-07-02 09:15:40 +0800

    feat(shell.ts): man

--- src/shell.ts
+++ src/shell.ts
@@ -14,7 +14,7 @@ async function exec(command: string) {
 }
 
 async function execNoShell(command: string, args?: string[]) {
-  logger.info('[shell] :', command, args?.length ? args : '');
+  logger.info('[shell] :', [command, ...(args ? args : [])]);
   const chunks = new Array<string>;
   const cp = childProcess.spawn(command, args);
   const readChunk = (chunk: any) => {
@@ -29,21 +29,32 @@ async function execNoShell(command: string, args?: string[]) {
 }
 
 async function execNoShellPlain(command: string, args?: string[]) {
-  return ansiCSIFilter(await execNoShell(command, args));
+  return ansiFilter(await execNoShell(command, args));
 }
 
 export async function fromContext(ctx: Context<Update.MessageUpdate<Message.TextMessage>>) {
-  const text = ctx.text.split(/\s+/).splice(1);
+  const text = ctx.text.split(/\s+/).slice(1);
   if (!text[0]) {
     ctx.reply('你不給咱命令运行个啥嘛! 咱又不能給你交互 Shell 喵...');
   } else if (text[0] === 'neofetch' 
     || text[0] === 'fortune'
     || text[0] === 'uptime'
     || text[0] === 'dig'
+    || text[0] === 'man'
     || text[0] === 'whois')
   {
-    const v = await execNoShellPlain(text[0], text.splice(1));
-    for (const c of toHtmlPreArray(v)) {
+    const v = await execNoShellPlain(text[0], text.slice(1));
+    if (v.length > 32767) {
+      ctx.sendChatAction('upload_document');
+      ctx.replyWithDocument({
+        source: Buffer.from(v),
+        filename: text.join('_') + '.txt'
+      }, {
+        caption: `Command output too long (${v.length} >32767)!\nHere is your output text document.`
+      });
+      logger.info('[shell]', text, 'output too long:', v.length);
+    }
+    else for (const c of toHtmlPreArray(v)) {
       await ctx.replyWithHTML(c);
     }
   } else {
@@ -52,7 +63,8 @@ export async function fromContext(ctx: Context<Update.MessageUpdate<Message.Text
 }
 
 const ANSI_CSI_REGEX = /\x1b\[[\x30-\x3f\x20-\x2f]*?[\x40-\x7e]/g;
+const ASCII_BS_REGEX = /.[\b]/g;
 
-function ansiCSIFilter(text: string) {
-  return text.replace(ANSI_CSI_REGEX, '');
+function ansiFilter(text: string) {
+  return text.replace(ANSI_CSI_REGEX, '').replace(ASCII_BS_REGEX, '');
 }
