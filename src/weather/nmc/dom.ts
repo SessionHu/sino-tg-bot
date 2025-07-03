e4062386d0433cff13cb88f3dd1bef48ec7a627a
SessionHu <102411014+SessionHu@users.noreply.github.com>
2025-07-03 13:09:41 +0800

    feat: weather raderURLs

---
+++ src/weather/nmc/dom.ts
@@ -0,0 +1,14 @@
+import { parse } from 'node-html-parser';
+
+import * as logger from '../../logger';
+import { HEADERS } from '.';
+
+export async function raderURLs(url: string): Promise<string[]> {
+  logger.info('[weather] [NMCl', url);
+  const htmltext = await fetch(url, {
+    headers: HEADERS
+  }).then(r => r.text());
+  const rootelem = parse(htmltext);
+  const timeWrap = rootelem.getElementById('timeWrap');
+  return timeWrap?.children.map(e => e.getAttribute('data-img')).filter(Boolean).reverse() as string[] ?? [];
+}
