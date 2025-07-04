c8994eefefe19e84bf041e13ecbed622095610bd
SessionHu <102411014+SessionHu@users.noreply.github.com>
2025-07-04 14:38:33 +0800

    feat(shell.ts): command /shell free

--- src/shell.ts
+++ src/shell.ts
@@ -66,6 +66,7 @@ export async function fromContext(ctx: Context<Update.MessageUpdate<Message.Text
     || text[0] === 'dig'
     || text[0] === 'man'
     || text[0] === 'date'
+    || text[0] === 'free'
     || text[0] === 'whois')
   {
     const v = await execNoShellPlain(text[0], text.slice(1));
